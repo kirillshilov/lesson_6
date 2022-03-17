c8b6fdd694d33018c50d7c5c87cfbfe3c61e68b3
kirillshilov <sarataza@inbox.ru>
2022-03-17 14:48:04 +0300

    commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,9 +1,56 @@
 package ru.skypro;
 
-public class Main {
+public class Main { int[] arr = generateRandomArray();
 
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
     public static void main(String[] args) {
-	// write your code here
+        {
+            // Задача 1
 
+            int sum = 0;
+            for (int element : generateRandomArray()) {
+                sum = sum + element;
+            }
+            System.out.println(sum);
+        }
+        {
+            // Задача 2
+
+            int sum = 0;
+            for (int element : generateRandomArray()) {
+                if (sum < element) {
+                    sum = element;
+                }
+            }
+            System.out.println( "Максимальная сумма трат за день составила " + sum +" рублей");
+            for (int element : generateRandomArray()){
+                sum = element;
+                if(sum > element){
+                    sum = element;
+        }
+            }
+            System.out.println( "Минимальная сумма трат за день составила " + sum +" рублей");
+        }
+        {
+            // Задача 3
+            float sum = 0;
+            for (float element : generateRandomArray()) {
+                sum = sum + element;
+            }
+            System.out.println("Средняя сумма трат за месяц составила " + sum/30 + "рублей");
+        }
+        {
+            char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+            for (int i = reverseFullName.length; i > 0; i--) {
+                System.out.print(reverseFullName [i-1]);
+            }
+        }
     }
 }
